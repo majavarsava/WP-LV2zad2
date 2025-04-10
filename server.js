fefa84c664e95ea2a456937a4a1ab90e7b247134
majavarsava <maja.varsava.4@gmail.com>
2025-04-10 11:00:29 +0200

    Added to server.js for opening the app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
 res.send('Pozdrav sa Railway servera!');
